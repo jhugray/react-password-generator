f8b8b3ca2f1426b44aced7075619535c3444b1fb
Jess Hause Ugray <jess.ugray@gmail.com>
2022-05-05 14:55:33 -0400

    initial file structure, component folder added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import PasswordGenerator from './components/passwordGenerator';
 
 function App() {
+  // return <div>Hello World333</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   
+    <PasswordGenerator />
+
   );
 }
 
---
+++ src/components/passwordGenerator/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class PasswordGenerator extends React.Component {
+  render() {
+    return (
+      <h1>Generate a password</h1>
+    )
+  }
+};
+
+export default PasswordGenerator;
