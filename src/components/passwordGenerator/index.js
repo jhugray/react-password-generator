0951c97dc4c4372e9d98c2d27e34db83e0caead7
Jess Hause Ugray <jess.ugray@gmail.com>
2022-05-06 13:33:20 -0400

    checklist component added

--- src/components/passwordGenerator/index.js
+++ src/components/passwordGenerator/index.js
@@ -1,11 +1,29 @@
 import React from "react";
 
+const passwordOptions = [
+  "Include upper case characters",
+  "Include lower case character",
+  "Include special characters"
+];
+
+
+
 class PasswordGenerator extends React.Component {
   render() {
-    return (
-      <h1>Generate a password</h1>
-    )
+  return (
+    <div className="checkList">
+      <h2 className="title">Password Options:</h2>
+      <ul className="list-container">
+        {passwordOptions.map((item, index) => (
+           <div key={index}>
+            <input value={item} type="checkbox" />
+            <span>{item}</span>
+           </div>
+        ))}
+      </ul>
+    </div>
+  );
   }
-};
+}
 
 export default PasswordGenerator;
